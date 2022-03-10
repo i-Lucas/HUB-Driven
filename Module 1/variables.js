0bb9ccaf7ec75a8ff7c2b7114b8b9e81d5845f93
i-Lucas <novo.contato.lucas@gmail.com>
2022-03-10 14:04:24 -0300

    update variables

--- Module 1/variables.js
+++ Module 1/variables.js
@@ -8,23 +8,21 @@
 const calculateArea = (r) => 3.1415 * (r*r)
 
 /* 
-Create a function called calculateVelocity(v0, a, t) that takes three parameters, representing the initial velocity, 
-acceleration, and instant of time of the motion of a particle, respectively, and returns the velocity of that particle, 
-at that instant t, from the formula for the time function of the movement:
+    Create a function called calculateVelocity(v0, a, t) that takes three parameters, representing the initial velocity, 
+    acceleration, and instant of time of the motion of a particle, respectively, and returns the velocity of that particle, 
+    at that instant t, from the formula for the time function of the movement:
 
      V = v0 + a*t
 
-Create a variable called speed that receives the value of the expression above, and return this variable.
+    Create a variable called speed that receives the value of the expression above, and return this variable.
 
-
-
-Crie uma função chamada calculaVelocidade(v0, a, t) que recebe três parâmetros, que representam respectivamente a velocidade inicial, 
-a aceleração e o instante de tempo do movimento de uma partícula e retorna a velocidade dessa partícula, nesse instante t, 
-a partir da fórmula da função horária do movimento:
+    Crie uma função chamada calculaVelocidade(v0, a, t) que recebe três parâmetros, que representam respectivamente a velocidade inicial, 
+    a aceleração e o instante de tempo do movimento de uma partícula e retorna a velocidade dessa partícula, nesse instante t, 
+    a partir da fórmula da função horária do movimento:
 
     V = v0 + a*t
 
-Crie uma variável chama velocidade que receba o valor da expressão acima, e retorne essa variável.
+    Crie uma variável chama velocidade que receba o valor da expressão acima, e retorne essa variável.
 */
 
 // solution
