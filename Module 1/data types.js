1692c8ca44dce2d4983f82e52339cb595d72d167
i-Lucas <novo.contato.lucas@gmail.com>
2022-03-10 13:42:38 -0300

    update data types

---
+++ Module 1/data types.js
@@ -0,0 +1,29 @@
+/*
+	Create a function called convertToNumber(text), 
+	which will receive a numeric text structure as a parameter 
+	and should return this transformed text to type number.
+
+	Crie uma função chamada converterParaNumber(texto), 
+	que vai receber uma estrutura de texto numérica como parâmetro 
+	e deve retornar esse texto transformado para o tipo number.
+*/
+
+
+// solution
+const convertToNumber = (text) => Number(text)
+
+
+// Create a function called returnType(input), which takes a parameter and manages to correctly return the type of that parameter.
+// Crie uma função chamada retornaTipo(entrada), que recebe um parâmetro e consegue retornar corretamente o tipo desse parâmetro.
+
+
+// solution
+const returnType = (x) => typeof(x)
+
+
+// Make a function called SpecialType() that takes no parameters but can respond with an output of type NaN
+// Faça uma função chamada, tipoEspecial(), que não recebe parâmetros mas que consegue responder com uma saída do tipo NaN
+
+
+// solution
+const SpecialType = () => NaN
