3d0d090bd94395e78d961a9f9d103992f95afd27
i-Lucas <novo.contato.lucas@gmail.com>
2022-03-10 13:39:47 -0300

    update functions

---
+++ Module 1/functions.js
@@ -0,0 +1,26 @@
+// Create a function called newFunction() that returns the text "Hello World"
+// Crie uma função chamada newFunction() que retorne o texto "Hello World"
+
+// solution
+const newFunction = () => 'Hello World'
+
+
+// Create a function called myAge that returns a number representing your age
+// Crie uma função chamada minhaIdade que retorna um número que represente a sua idade
+
+// solution
+const myAge = () => 25
+
+
+// Create a function called myFirstName that returns a text that represents your first name
+// Crie uma função chamada meuPrimeiroNome que retorna um texto que represente seu primeiro nome
+
+// solution
+const myFirstName = () => 'Lucas'
+
+
+// Write a function called FootballTeam(team) that takes the name of a football team and returns that name
+// Escreva uma função chamada timeDeFutebol(time) que recebe o nome de um time de futebol e retorna esse nome
+
+// solution
+const footballTeam = (time) => time
