3e4cc1666734fde2cbe6372c5b64b50e63714097
i-Lucas <novo.contato.lucas@gmail.com>
2022-04-14 05:18:02 -0300

    update manipulating array by index

--- Module 2/manipulating array by index.js
+++ Module 2/manipulating array by index.js
@@ -6,10 +6,7 @@
 		lst = [1, 6, 5, 4, 9, 8] => 4.5
 
 	Crie uma função chamada middleOne(lst), que recebe uma array de inteiros e retorna o valor do elemento central,
-	caso a array apresente um número par de elementos, retorne a média entre os dois elementos centrais.
-	Ex:
-	    lst = [1, 6, 5, 4, 9] => 5
-	    lst = [1, 6, 5, 4, 9, 8] => 4.5
+	caso a array apresente um número par de elementos, retorne a média entre os dois elementos centrais.5
 */
 
 
@@ -35,18 +32,16 @@ function middleOne(list) {
 
 
 /*
-	Create a function called insert(x, lst), which takes a number x and an ordered array of numbers and correctly inserts x at the proper position.
+	Create a function called insert(x, lst), which takes a number x and an ordered array of 
+	numbers and correctly inserts x at the proper position.
 	Ex:
 		x = 4, lst = [1, 2, 2, 2, 4, 5, 8] => Should return [1, 2, 2, 2, 4, 4, 5, 8]
 		x = 10, lst = [2, 4, 6, 8] => Should return [2, 4, 6, 8, 10]
 		x = 5, lst = [ ] => Should return [5]
 
 
-	Crie uma função chamada insere(x, lst), que recebe um número x e uma array ordenada de números e insere corretamente x na posição adequada.
-	Ex:
-	    x = 4, lst = [1, 2, 2, 2, 4, 5, 8] => Deve retornar [1, 2, 2, 2, 4, 4, 5, 8]
-	    x = 10, lst = [2, 4, 6, 8] => Deve retornar [2, 4, 6, 8, 10]
-	    x = 5, lst = [ ] => Deve retornar [5]
+	Crie uma função chamada insere(x, lst), que recebe um número x e uma array ordenada de números 
+	e insere corretamente x na posição adequada.
 */
 
 
@@ -57,17 +52,15 @@ function insert(x, list) {
 
 
 /*
-	Create a function called returnPoints(lst), which takes a list of numbers and returns the first and last elements of that list in a new list.
+	Create a function called returnPoints(lst), which takes a list of numbers and returns the first and 
+	last elements of that list in a new list.
 	Ex:
 		lst = [2, 3, 5, 7, 8] => Should return [2, 8]
 		lst = [ 3 ] => Should return [ 3 ]
 		lst = [ ] => Should return [ ]
 
-	Crie uma função chamada retornaPontas(lst), que recebe uma lista de números e retorna o primeiro e o último elemento dessa lista em uma nova lista.
-	Ex:
-	    lst = [2, 3, 5, 7, 8] => Deve retornar [2, 8]
-	    lst = [ 3 ] => Deve retornar [ 3 ]
-	    lst = [ ] => Deve retornar [ ]
+	Crie uma função chamada retornaPontas(lst), que recebe uma lista de números e 
+	retorna o primeiro e o último elemento dessa lista em uma nova lista.
 */
 
 
@@ -92,10 +85,6 @@ const returnPoints2 = (list) => list.length <= 1 ? [...list] : new Array(list[0]
 
 	Crie uma função chamada swapPoints(lst) que recebe uma lista e deve fazer uma troca de posições entre 
 	o primeiro e o último elemento, por fim deve retornar uma nova lista com esses elementos reordenados.
-	Ex:
-	    lst = [3, 2, 4, 5] => Deve retornar [5, 2, 4, 3]
-	    lst = [2] => Deve retornar [2]
-	    lst = [ ] => Deve retornar [ ]
 */
 
 
@@ -139,11 +128,6 @@ function swapPoints3(list) {
 
 	Crie a função chamada swapMenorNumero(lst) que recebe uma lista, percorre essa lista em busca do menor número, 
 	e realiza uma troca de posição entre o menor número e o primeiro elemento da array.
-	Ex:
-	    lst = [5, 6, 8, 4, 5, 9] => Deve retornar [4, 6, 8, 5, 5, 9]
-	    lst = [ 2 ] => Deve retornar [ 2 ]
-	    lst = [4, 5, 2, 4, 5, 2] => Deve retornar [2, 5, 4, 4, 5, 2]
-	    lst = [ ] => Deve retornar [ ]
 */
 
 
@@ -178,10 +162,6 @@ function swapSmallerNumber(list) {
 
 	Crie uma função chamada paresPares(lst) que recebe uma lista e deve retornar uma nova lista apenas com os 
 	elementos que são pares e que se encontram em índices pares na lista original.
-	Ex:
-	    lst = [0, 1, 2, 3, 4, 5] => Deve retornar [0, 2, 4]
-	    lst = [1, 2, 10, 12, 13, 4] => Deve retornar [10]
-	    lst = [] => Deve retornar []
 */
 
 
@@ -208,10 +188,6 @@ const pairsPairs2 = (list) => list.filter((number, index) => number % 2 === 0 &&
 
 	Crie uma função chamada desloca(lst), que recebe uma lista e retorna uma nova lista com os elementos 
 	deslocados uma posição a direita, e onde o último elemento da lista recebida será o primeiro da nova lista.
-	Ex:
-	    lst = [3, 4, 5, 6, 7] => Deve retornar [7, 3, 4, 5, 6]
-	    lst = [5, 'maça', 9, 'cajus', 15, 'uvas'] => Deve retornar ['uvas', '5, 'maça', 9, 'cajus', 15]
-	    lst = [ ] => Deve retornar vazia
 */
 
 
@@ -256,16 +232,15 @@ function displace3(list) {
 
 
 /*
-	Create a function called removeImpares(lst), which takes a list and returns a new list without the elements of the odd indices of the received list.
+	Create a function called removeImpares(lst), which takes a list and returns a 
+	new list without the elements of the odd indices of the received list.
 	Ex:
 		lst = [0, 1, 2, 3, 4, 5] => Should return [0, 2, 4]
 		lst = ['yes', 'no', 'yes', 'no', 'yes', 'no',] => Should return ['yes', 'yes', 'yes']
 
 
-	Crie uma função chamada removeImpares(lst), que recebe uma lista e retorna uma nova lista sem os elementos dos índices ímpares da lista recebida.
-	Ex:
-	    lst = [0, 1, 2, 3, 4, 5] => Deve retornar [0, 2, 4]
-	    lst = ['sim', 'não', 'sim', 'não', 'sim', 'não',] => Deve retornar ['sim', 'sim', 'sim']
+	Crie uma função chamada removeImpares(lst), que recebe uma lista e retorna uma 
+	nova lista sem os elementos dos índices ímpares da lista recebida.
 */
 
 
@@ -273,7 +248,6 @@ function displace3(list) {
 function removeOdd(list) {
     return list.filter(number => number % 2 !== 0)
 }
-
 
 
 // solution 2
